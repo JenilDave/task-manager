d10938967edf241928f09eee2ac48cb0e5707c4e
Jenil Dave <jnldv11@gmail.com>
2025-12-31 08:57:39 +0530

    Modification for SQLite batch update constraint

--- src/db/base.py
+++ src/db/base.py
@@ -1,8 +1,20 @@
-from sqlalchemy import func
+from sqlalchemy import func, MetaData
 from datetime import datetime, timezone
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+
 class Base(DeclarativeBase):
+
+    metadata = MetaData(naming_convention=convention)
+
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     created_at: Mapped[datetime] = mapped_column(default=lambda: datetime.now(timezone.utc),
         onupdate=func.now(),
