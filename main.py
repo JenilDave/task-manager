88cd54d6d861d5acf8b0cc9fe4f5cebd042d3a0a
Jenil Dave <jnldv11@gmail.com>
2026-01-01 22:21:56 +0530

    Add Project labels on swagger

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from src.api import task, user
 
-app = FastAPI()
+app = FastAPI(
+    title="Task Manager API",
+    description="API for managing tasks and their statuses",
+)
 
 app.include_router(task.router)
 app.include_router(user.router)
